c46908a9ffbbe0ba5786a9ddb1692374e2374fa7
oksanachernyh <oksanka.chernyh@gmail.com>
2026-02-09 18:04:21 +0300

    Add files via upload

---
+++ 2.9/src/test/java/pages/ReplenishmentBlock.java
@@ -0,0 +1,47 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+
+public class ReplenishmentBlock {
+    private WebDriver driver;
+
+    public ReplenishmentBlock(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getTitle() {
+        return driver.findElement(By.xpath("")).getText();
+    }
+
+    public List<WebElement> getPaymentSystemLogos() {
+        return driver.findElements(By.xpath(""));
+    }
+
+    public boolean isAboutServiceLinkDisplayed() {
+        WebElement link = driver.findElement(By.xpath(""));
+        return link.isDisplayed();
+    }
+
+    public void clickAboutServiceLink() {
+        driver.findElement(By.xpath("")).click();
+    }
+
+    public void fillServiceForm(String serviceType, String phoneNumber) {
+        driver.findElement(By.xpath("")).click();
+        WebElement phoneInput = driver.findElement(By.xpath(""));
+        phoneInput.clear();
+        phoneInput.sendKeys(phoneNumber);
+    }
+
+    public boolean isContinueButtonEnabled() {
+        WebElement btn = driver.findElement(By.xpath(""));
+        return btn.isEnabled();
+    }
+
+    public void clickContinue() {
+        driver.findElement(By.xpath("")).click();
+    }
+}
